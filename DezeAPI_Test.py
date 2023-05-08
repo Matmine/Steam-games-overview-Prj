b4ec7917cb8dc8759b0874fdf29f87afd9058045
MrDezem <107032969+MrDezem@users.noreply.github.com>
2023-05-08 13:47:32 +0000

    matthieu

--- DezeAPI_Test.py
+++ DezeAPI_Test.py
@@ -1,17 +1,39 @@
 import requests
+import csv
 
 # Remplacez VOTRE_CLE_API par votre clé d'API Steam
-url = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=730&key=F6A1445CAF0CDA9880436246FCB3E8AB'
+#url = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=730&key=F6A1445CAF0CDA9880436246FCB3E8AB'
+url = 'https://store.steampowered.com/api/appdetails?appids='
+game_tags = []
+api_key = "&key=F6A1445CAF0CDA9880436246FCB3E8AB"
+
 
 response = requests.get(url)
 
-if response.status_code == 417246:
-    data = response.json()
-    num_players = data['response']['player_count']
-    print(f"Nombre total de joueurs pour New world: {num_players}")
-else:
-    print("Erreur lors de la requête à l'API de Steam")
+with open('steam_data_mathis.csv', mode='r') as file:
+    csv_reader = csv.reader(file)
+    header = next(csv_reader)
+    game_tag = []
+    for row in csv_reader:
+        game_tags.append(row[0])
+
+
+for game_tag in game_tags :
+    url = 'https://store.steampowered.com/api/appdetails?appids='
+    url = url+game_tag+api_key
+    print(url)
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        game_info = data[game_tag]["data"]
+        print("Nom du jeu:", game_info["name"])
+        print("Description du jeu:", game_info["detailed_description"])
+    else:
+        print("Erreur lors de la requête à l'API de Steam")
 
 
 
 """F6A1445CAF0CDA9880436246FCB3E8AB"""
+
+
+
--- DezeTest.py
+++ DezeTest.py
@@ -1,6 +1,34 @@
-import pandas as pd
+import requests
+import csv
 
-df = pd.read_csv('steam_data_mathis.csv')
-df = df.sort_values(by='Current Players', ascending=False)
+# Remplacez VOTRE_CLE_API par votre clé d'API Steam
+#url = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=730&key=F6A1445CAF0CDA9880436246FCB3E8AB'
+url = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid='
+game_tags = []
+api_key = "&key=F6A1445CAF0CDA9880436246FCB3E8AB"
 
-print(df)
+
+"""for game_tag in game_tags :"""
+with open('steam_data_mathis.csv', mode='r') as file:
+    csv_reader = csv.reader(file)
+    header = next(csv_reader)
+    game_tag = []
+    for row in csv_reader:
+        game_tags.append(row[0])
+print(game_tags)
+
+for game_tag in game_tags :
+    url = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid='
+    url = url+game_tag+api_key
+    print(url)
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        num_players = data['response']['player_count']
+        print(f"Nombre total de joueurs pour New world: {num_players}")
+    else:
+        print("Erreur lors de la requête à l'API de Steam")
+
+
+
+"""F6A1445CAF0CDA9880436246FCB3E8AB"""
